e9847b83808382aae859ce33d7041568bb462102
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-12-21 16:10:49 -0800

    image

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby astronaut.png" }) {
+        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
           childImageSharp {
             fluid(maxWidth: 300) {
               ...GatsbyImageSharpFluid
