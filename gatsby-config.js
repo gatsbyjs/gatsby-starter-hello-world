0573d28506b60254cf86226fbcd26d0ef382e1d0
Victor Mithouard <victor@thegoodtrends.com>
2021-03-01 21:05:20 -0500

    Updated plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,43 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+	plugins: [{
+		resolve: 'gatsby-source-bigcommerce',
+		options: {
+			clientId: 'mwmzcbp13yp1s6ajmfl4v62ffvzy647',
+			secret: '96tzy8jrt9es4zawebcpx4un08notxz',
+			accessToken: '3kivmg0g3fyos4sbqo3fk4pvj0sp66w',
+			storeHash: '42s5es9otw',
+			endpoint: '/catalog/products',
+
+			// OPTIONAL
+			logLevel: 'info',
+			nodeName: 'BigCommerceNode',
+			apiVersion: 'v3',
+
+			// Multiple endpoints in an object.
+			endpoints: {
+				BigCommerceProducts: "/catalog/products",
+				BigCommerceCategories: "/catalog/categories",
+			}
+		}
+	},	
+	{
+	    resolve: `gatsby-source-airtable`,
+	    options: {
+	      apiKey: `keyUM7mD5f4hp8yh0`, // may instead specify via env, see below
+	      concurrency: 5, // default, see using markdown and attachments for more information
+	      tables: [
+	        {
+	          baseId: `app1K0wx2fQSngaRX`,
+	          tableName: `product_catalog`,
+	          // can leave off queryName, mapping or tableLinks if not needed
+	        }
+	      ]
+	    }
+	  },
+	  `gatsby-transformer-sharp`, 
+	  `gatsby-plugin-sharp`
+
+	],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,39 @@
-import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+
+
+export default function Home({data}) {
+	return ( 
+		<div > Hello world people! 
+		{data.allAirtable.edges.map(({ node }) => (
+            <>
+	            <h1>{node.data.name}</h1>
+	            <img src={node.data.image_url_1} />
+	        </>
+		))}
+		</div>
+	)
 }
+
+export const query = graphql`
+  {
+    allAirtable(limit: 20) {
+      totalCount
+      pageInfo {
+        currentPage
+        hasNextPage
+      }
+      edges {
+        node {
+          data {
+            image_url_1
+            image_url_2
+            name
+          }
+        }
+      }
+    }
+  }
+`
