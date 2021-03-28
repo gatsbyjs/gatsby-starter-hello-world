ce6ee4a6bd1294690610bfb84d49370842df7e2f
salesthegoodtrends <77804027+salesthegoodtrends@users.noreply.github.com>
2021-03-28 12:21:50 -0400

    Create gatsby-ssr.js

---
+++ gatsby-ssr.js
@@ -0,0 +1,53 @@
+// Css import of global styles
+import "./src/styles/global.css"
+import React from 'react';
+// Stripe
+//import { loadStripe } from '@stripe/stripe-js'
+import { CartProvider } from 'use-shopping-cart'
+import getStripe from "./src//utils/stripejs"
+// Fauna DB
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/client';
+import { graphql } from "gatsby";
+//import { ApolloClient, ApolloLink, createHttpLink } from '@apollo/client';
+//import fetch from 'isomorphic-fetch';
+const { GRAPHQL_URL } = process.env
+
+
+
+
+// Initiate Stripe
+const stripePromise = getStripe()
+
+// Initializes the Apollo Client
+export const query = graphql`
+  query PageQuery {
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`
+// Initiate Apollo
+  const client = new ApolloClient({
+    uri: GRAPHQL_URL,
+  })
+
+export const wrapRootElement = ({ element }) => {
+  return (
+  <ApolloProvider client={client} >
+    <CartProvider mode="checkout-session" stripe={stripePromise} currency="USD">
+		  		
+		  {element}
+		
+   	</CartProvider>
+  </ApolloProvider>
+  	);
+}
+
+
+
+
+
+
