7f28599bf21152bd77df4b6b584c3b6de9f6bd2d
salesthegoodtrends <77804027+salesthegoodtrends@users.noreply.github.com>
2021-04-06 20:00:23 -0400

    Update

--- functions/order_confirmation.js
+++ functions/order_confirmation.js
@@ -1,54 +1,38 @@
+const { logOrder } = require("./utils/airtable")
 
+const HEADER = {
+	"Content-Type": "application/json",
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Headers": "Content-Type,X-XSRF-TOKEN,X-CSRF-TOKEN",
+	"Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
 
-
-const { logOrder } = require('./utils/airtable')
-
-const HEADER = {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers' : 'Content-Type,X-XSRF-TOKEN,X-CSRF-TOKEN'}
+}
 
 /* Creates the log of the order in AT then creates the orer in Bigcommerce */
 exports.handler = async (event, context, callback) => {
-	var resp;
-	let order=JSON.parse(event.body);
-	resp= await logOrder(order);
+	var resp
+	let order = JSON.parse(event.body)
+	resp = await logOrder(order)
+	
 	/* -- Order is saved in AT --*/
-	if (typeof resp !== 'undefined') {
-		
+	/* -- Workflow is triggered in Pipedream from AT --*/
+	
 
-
-
-
+	if (typeof resp !== "undefined") {
 
 		sendBack = {
 			statusCode: 200,
 			headers: HEADER,
-			body: JSON.stringify(resp)
+			body: JSON.stringify(resp),
+		}
+
+		return sendBack
+	} else {
+		sendBack = {
+			statusCode: 400,
+			headers: HEADER,
+			body: "No order was created",
 		}
 		return sendBack
-		}	
-		
-		/* Create the cart */
-
-
-
-
-
-	/* Places the order with the cart created - 2s delay to allow for coupon to create */
-
-
-
-	else {
-	sendBack = {
-		statusCode: 400,
-		headers: HEADER,
-		body: "No order was created"
 	}
-	return sendBack
-	}
-
-
-
-
-
-
-	
 }
