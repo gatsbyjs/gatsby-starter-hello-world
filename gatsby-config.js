601ed4ebc7e20acc367cfe92e75f105920be3aa4
salesthegoodtrends <77804027+salesthegoodtrends@users.noreply.github.com>
2021-04-09 10:53:11 -0400

    Updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -142,7 +142,7 @@ module.exports = {
 			    resolve: `gatsby-plugin-netlify`,
 			    options: {
 			      headers: {
-			      	'Content-Type': ['application/json'], 
+			      	'Content-Type': ['*'], 
 			      	'Access-Control-Allow-Origin': ['*'], 
 			      	'Access-Control-Allow-Headers' : ['Content-Type,X-XSRF-TOKEN,X-CSRF-TOKEN']}, // option to add more headers. `Link` headers are transformed by the below criteria
 			      allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
